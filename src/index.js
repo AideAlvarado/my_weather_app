70bfbad05d934e54a52708f4c46b55cfb26ba5f1
AideAlvarado <78866513+AideAlvarado@users.noreply.github.com>
2021-02-19 23:05:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,113 +1,106 @@
-/*let weather = [
-  { name: "paris", temp: 19.7, humidity: 80 },
-  { name: "tokio", temp: 17.3, humidity: 50 },
-  { name: "lisbon", temp: 30.2, humidity: 20 },
-  { name: "san francisco", temp: 20.9, humidity: 100 },
-  { name: "moscow", temp: -5, humidity: 20 }
-];
-
-function minusculas(linea) {
-  let respuesta = linea.toLowerCase();
-  respuesta = linea.trim();
-  return respuesta;
+function zfill(num, len) {
+  return (Array(len).join("0") + num).slice(-len);
 }
-*/
+function showTime() {
+  let now = new Date();
+  let date = now.getDate();
+  let hours = zfill(now.getHours(), 2);
+  let minutes = zfill(now.getMinutes(), 2);
 
-// Para mostrar la fecha y hra actual:
-let now = new Date();
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[now.getDay()];
+  let year = now.getFullYear();
+  let texto = document.getElementById("weatherdate");
+  texto.innerHTML = ` ${day}, ${hours}:${minutes}`;
+}
+function displayForecast(response) {
+  let forecast = document.getElementById("forecast").querySelectorAll(".col3");
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = [now.getDay()];
-let year = now.getFullYear();
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec"
-];
-let Month = months[now.getMonth()];
-let texto = document.getElementById("fecha");
-texto.innerHTML = ` ${Month} ${date} ${year}, ${hours}:${minutes}`;
+  for (let i = 0; i < 5; i++) {
+    let hour = response.data.list[i].dt_txt.split(" ")[1].slice(0, 5);
+    let tmax = Math.round(response.data.list[i].main.temp_max);
+    let tmin = Math.round(response.data.list[i].main.temp_min);
+    let wicon = `<img src="http://openweathermap.org/img/wn/${response.data.list[i].weather[0].icon}@2x.png">`;
 
-//Para obtener la ciudad y mostrarla en el h1:
-/*
-  try {
-    apiRes = await axios.get('https://silex.edgeprop.my/api/v1/a');
-  } catch (err) {
-    apiRes = err.response;
-  } finally {
-    console.log(apiRes); // Could be success or error
+    forecast[
+      i
+    ].innerHTML = `<p>${hour}</p>${wicon}<p>${tmin}ºC / ${tmax}ºC</p>`;
   }
-*/
+}
+
 function city(event) {
   event.preventDefault();
   let showCity = document.getElementById("form-city");
-  let destino = document.getElementById("ciudaddestino");
+  let destino = document.getElementById("destinationcity");
   let key = "09c4248a521300b214dab61c69efdff2";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${showCity.value}&appid=${key}&units=metric`;
   destino.innerHTML = `${showCity.value}`;
-  console.log(`url-->${url}`);
+
   axios.get(url).then(showTemperature2);
-  console.log(`--> ciudad :${showCity.value}`);
 }
-let showCity = document.getElementById("search");
-showCity.addEventListener("click", city);
-let geol = document.getElementById("geol");
-geol.addEventListener("click", getCurrentPosition);
-//Obtener Temp de ciudad destino
 
-function showTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
-
-  let temperature = Math.round(response.data.main.temp);
-  let showTemp = document.getElementById("temp");
-  showTemp.innerHTML = `${temperature}`;
-}
 function showTemperature2(response) {
-  console.log(response);
-  console.log(response.data);
+  const conditions = {
+    Thunderstorm: "stormy",
+    Drizzle: "rainy",
+    Rain: "rainy",
+    Snow: "snowy",
+    Atmosphere: "stormy",
+    Clear: "sunny",
+    Clouds: "cloudy",
+    Mist: "cloudy",
+    Smoke: "stormy",
+    Haze: "rainy",
+    Dust: "cloudy",
+    Fog: "cloudy",
+    Sand: "stormy",
+    Ash: "stormy",
+    Squall: "stormy",
+    Tornado: "stormy"
+  };
+
   let temperature = Math.round(response.data.main.temp);
   let cityName = response.data.name;
-  console.log(`citiname -> ${cityName}`);
-  console.log(`->data`, response.data);
   let showTemp = document.getElementById("temp");
-  let showCity = document.getElementById("ciudaddestino");
+  let showCity = document.getElementById("destinationcity");
   showTemp.innerHTML = `${temperature}`;
+  console.log(cityName.length);
   showCity.innerHTML = `${cityName}`;
-  document.getElementById("infoHum").innerHTML = `Humidity ${Math.round(
+  let letraC = document.getElementById("degreesC");
+  let letraF = document.getElementById("degreesF");
+  letraC.style.fontSize = "x-large";
+  letraF.style.fontSize = "medium";
+  let target = document.getElementById("temp");
+  target.className = "celsius";
+  document.getElementById("infoHum").innerHTML = `humidity </br>${Math.round(
     response.data.main.humidity
   )}%`;
-  document.getElementById("infoWin").innerHTML = `Wind ${Math.round(
+  document.getElementById("infoWin").innerHTML = `wind </br>${Math.round(
     response.data.wind.speed
   )} Km/h`;
-  document.getElementById("infoPre").innerHTML = `Precipitation ${Math.round(
-    response.data.wind.speed
-  )}% `;
+
+  document.getElementById(
+    "Description"
+  ).innerHTML = `<h3>right now there</h3>${response.data.weather[0].description}`;
+  let claseClima = conditions[response.data.weather[0].main];
+
+  let weatherIcon = document.getElementById("weatherIcon");
+
+  weatherIcon.className = claseClima;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+  showTime();
 }
-let key = "09c4248a521300b214dab61c69efdff2";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=monterrey&appid=${key}&units=metric`;
-let showTemp = document.getElementById("temp");
-axios.get(url).then(showTemperature);
 
 //geolocalización
 function showPosition(position) {
@@ -115,7 +108,7 @@ function showPosition(position) {
   let latitude = position.coords.latitude;
   let key = "09c4248a521300b214dab61c69efdff2";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
-  console.log(url);
+
   axios.get(url).then(showTemperature2);
 }
 
@@ -127,58 +120,46 @@ function getCurrentPosition() {
 //Para convertir de Celsius a F y viceversa con un click:
 function clickOnC(event) {
   event.preventDefault();
-  console.log(event);
-  console.log(celsius);
 
-  let objetivo = document.getElementById("temp");
-  let cTemp = objetivo.innerHTML;
+  let target = document.getElementById("temp");
+  let cTemp = target.innerHTML;
   let letraC = document.getElementById("degreesC");
   let letraF = document.getElementById("degreesF");
-  console.log(`cTemp es ${cTemp}`);
-  console.log(letraC);
 
   let cToFahr = (cTemp * 9) / 5 + 32;
   let fToCelsius = Math.round(((cTemp - 32) * 5) / 9);
   if (event.srcElement.className === "celsius") {
-    letraC.style.fontSize = "30px";
-    letraF.style.fontSize = "20px";
-    if (objetivo.className === "celsius") {
-      console.log("no hago nada");
-    } else {
-      objetivo.innerHTML = fToCelsius;
-      objetivo.className = "celsius";
+    letraC.style.fontSize = "x-large";
+    letraF.style.fontSize = "medium";
+    if (target.className === "fahrenheit") {
+      target.innerHTML = fToCelsius;
+      target.className = "celsius";
     }
   } else {
-    letraC.style.fontSize = "20px";
-    letraF.style.fontSize = "30px";
-    if (objetivo.className === "celsius") {
-      objetivo.innerHTML = cToFahr;
-      objetivo.className = "fahrenheit";
-    } else {
-      console.log("no hago nada");
+    letraC.style.fontSize = "medium";
+    letraF.style.fontSize = "x-large";
+    if (target.className === "celsius") {
+      target.innerHTML = cToFahr;
+      target.className = "fahrenheit";
     }
   }
 }
+
+let showCity = document.getElementById("search");
+showCity.addEventListener("click", city);
+let geol = document.getElementById("geol");
+geol.addEventListener("click", getCurrentPosition);
+
+//Obtener Temp de ciudad destino
+
+let key = "09c4248a521300b214dab61c69efdff2";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=monterrey&appid=${key}&units=metric`;
+//let showTemp = document.getElementById("temp");
+axios.get(url).then(showTemperature2);
+
 let celsius = document.getElementById("degreesC");
-console.log(celsius.innerHTML);
-console.log(celsius);
 celsius.addEventListener("click", clickOnC);
 let fahrenheit = document.getElementById("degreesF");
-console.log(fahrenheit);
+
 fahrenheit.addEventListener("click", clickOnC);
-
-/*let question = prompt(`Enter a city?`);
-question = minusculas(question);
-
-let ciudad = weather.find((element) => element.name === question);
-if (ciudad === undefined) {
-  alert(
-    `Sorry, we don't know the weather for this city. Try going to  https://www.google.com/search?q=weather+${question}`
-  );
-} else {
-  alert(
-    `It is currently ${Math.round(ciudad.temp)}°C (${cToF(ciudad.temp)}°F) in ${
-      ciudad.name
-    } with a humidity of ${ciudad.humidity}%`
-  );
-}*/
+showTime();
